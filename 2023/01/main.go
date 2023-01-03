163dec71525313ea283614f72aa58c2dfa1bb314
17hao <sqh1107@gmail.com>
2023-01-03 22:13:54 +0800

    2023-1-1

---
+++ 2023/01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/trace"
+)
+
+// go run main.go
+// go tool trace trace.out
+func main() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(f *os.File) {
+		err = f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(f)
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+
+	fmt.Println("hello, world!")
+}
