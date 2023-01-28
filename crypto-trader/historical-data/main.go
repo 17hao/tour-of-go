12cc3dd9e9b3678192d2b8041c09f580de4bec60
17hao <sqh1107@gmail.com>
2023-01-28 18:46:05 +0800

    download historical k-lines

--- crypto-trader/historical-data/main.go
+++ crypto-trader/historical-data/main.go
@@ -7,43 +7,102 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/alexmullins/zip"
 	"github.com/pkg/errors"
 )
 
+var historicalDataDir = "/tmp/crypto-historical"
+
 // https://github.com/binance/binance-public-data/
 // https://www.binance.com/en/support/faq/how-to-download-historical-market-data-on-binance-5810ae42176b4770b880ce1f14932262
 func main() {
-	//daysEachMonth := map[int]int{
-	//	1:  31,
-	//	2:  28,
-	//	3:  31,
-	//	4:  30,
-	//	5:  31,
-	//	6:  30,
-	//	7:  31,
-	//	8:  31,
-	//	9:  30,
-	//	10: 31,
-	//	11: 30,
-	//	12: 31,
-	//}
-	//startYear := 2020
-	//startMonth := 1
-	//startDay := 1
-	//
-	//for i := startYear; i <= 2022; i++ {
-	//	for j := startMonth; j <= 12; j++ {
-	//		for k := startDay; k <= daysEachMonth[j]; k++ {
-	//			//fmt.Printf("https://data.binance.vision/data/spot/daily/klines/ETHUSDT/1s/ETHUSDT-1s-2019-01.zip")
-	//			fmt.Printf("ETHUSDT-1s-%02d-%02d-%02d.zip\n", i, j, k)
-	//		}
-	//	}
-	//}
+	if _, err := os.Stat(historicalDataDir); errors.Is(err, os.ErrNotExist) {
+		err = os.Mkdir(historicalDataDir, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
-	downloadZip("file:///Users/bytedance/Downloads/organizations-10000.zip")
-	unzip("/tmp/organizations-10000.zip")
+	daysEachMonth := map[int]int{
+		1:  31,
+		2:  28,
+		3:  31,
+		4:  30,
+		5:  31,
+		6:  30,
+		7:  31,
+		8:  31,
+		9:  30,
+		10: 31,
+		11: 30,
+		12: 31,
+	}
+	startYear := 2023
+	startMonth := 1
+	startDay := 27
+	dailyETHBaseURL := "https://data.binance.vision/data/spot/daily/klines/ETHUSDT/1d"
+	for i := startYear; i <= 2023; i++ {
+		for j := startMonth; j <= 12; j++ {
+			for k := startDay; k <= daysEachMonth[j]; k++ {
+				dailyETH := fmt.Sprintf("ETHUSDT-1d-%02d-%02d-%02d.zip", i, j, k)
+				url := fmt.Sprintf("%s/%s", dailyETHBaseURL, dailyETH)
+				path := downloadZip(url)
+				unzip(path)
+			}
+		}
+	}
+}
+
+// download zip file and write to /tmp/crypto-historical
+func downloadZip(url string) string {
+	c := http.Client{}
+	addFileProtocol(&c)
+	resp, err := c.Get(url)
+	if err != nil {
+		log.Fatal(errors.WithStack(err))
+	}
+	defer func(body io.ReadCloser) {
+		err := body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("download zip file failed, url=%s\n", url)
+	}
+
+	tmp := strings.Split(url, "/")
+	fileName := tmp[len(tmp)-1]
+	path := fmt.Sprintf("%s/%s", historicalDataDir, fileName)
+	if _, err := os.Stat(path); errors.Is(err, os.ErrExist) {
+		return path
+	}
+	zipFile, err := os.Create(path)
+	if err != nil {
+		log.Fatal(errors.WithStack(err))
+	}
+	defer func(zipFile *os.File) {
+		err := zipFile.Close()
+		if err != nil {
+
+		}
+	}(zipFile)
+
+	_, err = io.Copy(zipFile, resp.Body)
+	if err != nil {
+		log.Fatal(errors.WithStack(err))
+	}
+
+	return path
+}
+
+func addFileProtocol(c *http.Client) {
+	tr := &http.Transport{}
+	tr.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
+	c.Transport = tr
 }
 
 func unzip(path string) {
@@ -70,65 +129,9 @@ func unzip(path string) {
 		return
 	}
 
-	reader := bufio.NewReader(rc)
-	for i := 0; i < 10; i++ {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(errors.WithStack(err))
-		}
-		fmt.Print(line)
-	}
-
-	bs, err := io.ReadAll(rc)
-	if err != nil {
-		log.Fatal(errors.WithStack(err))
-		return
-	}
-	fmt.Println(string(bs))
-}
-
-func addFileProtocol(c *http.Client) {
-	tr := &http.Transport{}
-	tr.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
-	c.Transport = tr
-}
-
-// download zip file and write to /tmp
-func downloadZip(url string) {
-	c := http.Client{}
-	addFileProtocol(&c)
-	resp, err := c.Get(url)
-	if err != nil {
-		log.Fatal(errors.WithStack(err))
-		return
-	}
-	defer func(body io.ReadCloser) {
-		err := body.Close()
-		if err != nil {
-
-		}
-	}(resp.Body)
-
-	if resp.StatusCode != 200 {
-		log.Fatal("download zip file failed.")
-		return
-	}
-
-	zipFile, err := os.Create("/tmp/organizations-10000.zip")
-	if err != nil {
-		log.Fatal(errors.WithStack(err))
-		return
-	}
-	defer func(zipFile *os.File) {
-		err := zipFile.Close()
-		if err != nil {
-
-		}
-	}(zipFile)
-
-	_, err = io.Copy(zipFile, resp.Body)
-	if err != nil {
-		log.Fatal(errors.WithStack(err))
-		return
+	// buffered io
+	scanner := bufio.NewScanner(rc)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
 }
