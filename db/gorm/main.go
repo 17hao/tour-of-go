4cbc01cd8c8392ee3b6f4975d6d4efe85a67f715
shiqihao <shiqihao@bytedance.com>
2023-02-21 21:18:16 +0800

    gorm mock

--- db/gorm/main.go
+++ db/gorm/main.go
@@ -14,13 +14,23 @@ type employee struct {
 }
 
 func main() {
+	employees := query()
+	for _, e := range employees {
+		fmt.Printf("%+v\n", e)
+	}
+}
+
+func query() []employee {
 	db, err := gorm.Open(mysql.Open("shiqihao:123456@tcp(47.102.157.109:3306)/my_db"))
 	if err != nil {
 		panic(err)
 	}
 
-	var e employee
+	//var e employee
 	var es []employee
+
+	db.Raw("select id, name, city from employees where id > ?", 1).Scan(&es)
+	return es
 
 	//db.Raw("select id, name, city from employees where id = ?", 9).Scan(&e)
 	//fmt.Printf("e=%+v\n", e)
@@ -41,11 +51,11 @@ func main() {
 	//	fmt.Printf("%+v\n", e)
 	//}
 
-	db.Where("city <> ?", "Shanghai").First(&e)
-	fmt.Printf("e = %+v\n", e)
+	//db.Where("city <> ?", "Shanghai").First(&e)
+	//fmt.Printf("e = %+v\n", e)
 
-	db.Where("city <> ?", "Shanghai").Find(&es)
-	for _, i := range es {
-		fmt.Printf("%+v\n", i)
-	}
+	//db.Where("city <> ?", "Shanghai").Find(&es)
+	//for _, i := range es {
+	//	fmt.Printf("%+v\n", i)
+	//}
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.16
 
 require (
 	github.com/17hao/echo_server v1.0.2
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/Knetic/govaluate v3.0.0+incompatible
-	github.com/alexmullins/zip v0.0.0-20180717182244-4affb64b04d0
 	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220418222510-f25a4f6275ed
 	github.com/apache/thrift v0.16.0
+	github.com/bytedance/mockey v1.1.1
 	github.com/bytedance/sonic v1.0.2
 	github.com/cloudwego/kitex v0.4.4
 	github.com/emirpasic/gods v1.12.0
@@ -15,6 +16,7 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/wire v0.5.0
+	github.com/magiconair/properties v1.8.5
 	github.com/nyaruka/phonenumbers v1.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/shopspring/decimal v1.3.1
@@ -23,5 +25,5 @@ require (
 	github.com/umpc/go-sortedmap v0.0.0-20180422175548-64ab94c482f4
 	golang.org/x/text v0.3.7
 	gorm.io/driver/mysql v1.4.4
-	gorm.io/gorm v1.24.2
+	gorm.io/gorm v1.24.5
 )
