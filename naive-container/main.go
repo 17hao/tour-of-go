d3aec3cabbc1243b52cee66aa0fccd4945f1b6f8
17hao <sqh1107@gmail.com>
2023-11-12 21:27:25 +0800

    set new user namespace

--- naive-container/main.go
+++ naive-container/main.go
@@ -23,8 +23,22 @@ func run() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
 		Unshareflags: syscall.CLONE_NEWNS,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 	if err := cmd.Start(); err != nil {
 		logrus.WithField("stack", string(debug.Stack())).Errorf("ERR: %v", err)
